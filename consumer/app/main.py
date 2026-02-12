d4a8d46eccb92a52889cdf99579118e5dffbbf9a
JosephRechdiner <jrechdiner2002@gmail.com>
2026-02-12 10:39:07 +0200

    added dockerfile

--- consumer/app/main.py
+++ consumer/app/main.py
@@ -3,10 +3,8 @@ from kafka_consumer import listen
 
 def main():
     sql_manager = SQLManager()
+    sql_manager.init_db()
     listen(sql_manager)
-
-
     
-
 if __name__ == "__main__":
     main()
