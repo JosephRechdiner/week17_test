c324ae78aafb0af6d65f5b5216db968edf883888
JosephRechdiner <jrechdiner2002@gmail.com>
2026-02-12 10:53:16 +0200

    added routes.py

---
+++ analytics-api/app/routes.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Request, HTTPException, Depends
+from dal import Dal
+
+analytics_router = APIRouter()
+
+def get_sql_manager(reauest: Request):
+    return reauest.app.state.sql_manager
+
+@analytics_router.get("/analytics/top-customers")
+def get_top_customers(sql_manager = Depends(get_sql_manager)):
+    cnx = sql_manager.get_cnx()
+    customers = Dal.get_top_customer(cnx)
+    if not customers:
+        raise HTTPException(status_code=404, detail=f"Not found")
+    return customers
+
+@analytics_router.get("/analytics/customers-without-orders")
+def get_customers_without_orders(sql_manager = Depends(get_sql_manager)):
+    cnx = sql_manager.get_cnx()
+    customers = Dal.get_customers_without_orders(cnx)
+    if not customers:
+        raise HTTPException(status_code=404, detail=f"Not found")
+    return customers
+
+@analytics_router.get("/analytics/zero-credit-active-customers")
+def get_zero_credit_active_customers(sql_manager = Depends(get_sql_manager)):
+    cnx = sql_manager.get_cnx()
+    customers = Dal.get_zero_credit_active_customers(cnx)
+    if not customers:
+        raise HTTPException(status_code=404, detail=f"Not found")
+    return customers
+
